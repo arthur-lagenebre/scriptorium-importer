e06b78e492d5dd8eed9b921a0c0a1d46b6fea156
Arthur Lagenebre <lagenebre.arthur@gmail.com>
2024-09-02 13:55:44 +0200

    Reorder Card

--- MTG.Importer.Models/Card/Card.cs
+++ MTG.Importer.Models/Card/Card.cs
@@ -1,3 +1,3 @@
 ﻿namespace MTG.Importer.Models.Card;
 
-public record Card(Guid OracleId, Cost Cost, Name Name, Text Text, Typeline Typeline, Color Colors, Color ColorsIdentity, Color ColorsIndicator, string Layout, List<string> Keyword, List<string> ProducedMana, Set Set, List<Face> CardFaces, List<RelatedCard> RelatedCards, Creature? Creature, Planeswalker? Planeswalker, Vanguard? Vanguard);
+public record Card(Guid OracleId, Name Name, Typeline Typeline, Text Text, Cost Cost, Color Colors, Color ColorsIdentity, Color ColorsIndicator, string Layout, List<string> Keyword, List<string> ProducedMana, Set Set, List<Face> CardFaces, List<RelatedCard> RelatedCards, Creature? Creature, Planeswalker? Planeswalker, Vanguard? Vanguard);
--- MTG.Scryfall.Importer/Builders/NormalBuilder.cs
+++ MTG.Scryfall.Importer/Builders/NormalBuilder.cs
@@ -52,7 +52,7 @@ public class NormalBuilder : IScryfallBuilder
 
     public Card Build()
     {
-        var card = new Card(_oracleId, _cost, _name, _text, _typeline, _color, _colorIdentity, _colorIndicator, Layout.Name, _keywords, _producedMana, _set, _cardFaces, _relatedCards, _creature, _planeswalker, _vanguard);
+        var card = new Card(_oracleId, _name, _typeline, _text, _cost, _color, _colorIdentity, _colorIndicator, Layout.Name, _keywords, _producedMana, _set, _cardFaces, _relatedCards, _creature, _planeswalker, _vanguard);
 
         Reset();
 
