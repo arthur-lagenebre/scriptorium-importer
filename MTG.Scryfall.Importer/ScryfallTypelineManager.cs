3102e8224211e17c2716d972817536961211c4ce
Swarmotte <lagenebre.arthur@gmail.com>
2024-09-10 13:40:14 +0200

    Extract Properties typeline in a object

--- MTG.Scryfall.Importer/ScryfallTypelineManager.cs
+++ MTG.Scryfall.Importer/ScryfallTypelineManager.cs
@@ -1,17 +1,21 @@
 ﻿using MTG.Importer.Models.Card;
 using MTG.Scryfall.Importer.Helpers;
 using MTG.Scryfall.Importer.Interfaces;
+using MTG.Scryfall.Models;
 
 namespace MTG.Scryfall.Importer;
 
 public class ScryfallTypelineManager : IScryfallTypelineManager
 {
-    public const string _typelineSeparator = "—";
-    public const string _subtypeSeparator = " ";
-    public static readonly char[] _typeSeparators = [' '];
-    public static readonly List<string> _subtypes = ["New Phyrexia", "The Abyss", "Serra's Realm", "Bolas's Meditation Realm"];
-    public static readonly List<string> _supertypes = ["Basic", "Host", "Elite", "Legendary", "Ongoing", "Snow", "Token", "World"];
-    public static readonly List<string> _types = ["Artifact", "Battle", "Conspiracy", "Creature", "Emblem", "Enchantment", "Hero", "Instant", "Land", "Phenomenon", "Plane", "Planeswalker", "Scheme", "Sorcery", "Tribal", "Vanguard"];
+    private readonly TypelineProperties _properties; 
+
+    public ScryfallTypelineManager()
+    {
+        var subtypes = new List<string> { "New Phyrexia", "The Abyss", "Serra's Realm", "Bolas's Meditation Realm" };
+        var supertypes = new List<string> { "Basic", "Host", "Elite", "Legendary", "Ongoing", "Snow", "Token", "World" };
+        var types =  new List<string> { "Artifact", "Battle", "Conspiracy", "Creature", "Emblem", "Enchantment", "Hero", "Instant", "Land", "Phenomenon", "Plane", "Planeswalker", "Scheme", "Sorcery", "Tribal", "Vanguard"};
+        _properties = new TypelineProperties("-", " ", [' '], subtypes, supertypes, types);
+    }
 
     public Typeline ExtractTypeline(string? typeline)
     {
@@ -24,42 +28,42 @@ public class ScryfallTypelineManager : IScryfallTypelineManager
         return new Typeline(types, supertypes, subtypes);
     }
 
-    private static List<string> GetTypes(string typeline)
+    private List<string> GetTypes(string typeline)
     {
         if (!string.IsNullOrEmpty(typeline))
         {
-            typeline = typeline.Split(_typelineSeparator)[0].Trim();
+            typeline = typeline.Split(_properties.TypelineSeparator)[0].Trim();
 
-            return _types.Intersect(typeline.Split(_typeSeparators), StringComparer.OrdinalIgnoreCase).ToList();
+            return _properties.Types.Intersect(typeline.Split(_properties.TypeSeparators), StringComparer.OrdinalIgnoreCase).ToList();
         }
 
         return [];
     }
 
-    private static List<string> GetSupertypes(string typeline)
+    private List<string> GetSupertypes(string typeline)
     {
         if (!string.IsNullOrEmpty(typeline))
         {
-            typeline = typeline.Split(_typelineSeparator)[0].Trim();
+            typeline = typeline.Split(_properties.TypelineSeparator)[0].Trim();
 
-            return _supertypes.Intersect(typeline.Split(_typeSeparators), StringComparer.OrdinalIgnoreCase).ToList();
+            return _properties.Supertypes.Intersect(typeline.Split(_properties.TypeSeparators), StringComparer.OrdinalIgnoreCase).ToList();
         }
 
         return [];
     }
 
-    private static List<string> GetSubtypes(string typeline)
+    private List<string> GetSubtypes(string typeline)
     {
-        if (!string.IsNullOrEmpty(typeline) && typeline.Contains(_typelineSeparator))
+        if (!string.IsNullOrEmpty(typeline) && typeline.Contains(_properties.TypelineSeparator))
         {
-            var subtype = typeline.Split(_typelineSeparator)[1].Trim();
+            var subtype = typeline.Split(_properties.TypelineSeparator)[1].Trim();
 
-            if (_subtypes.Contains(subtype))
+            if (_properties.Subtypes.Contains(subtype))
             {
                 return [subtype];
             }
 
-            return [.. subtype.Split(_subtypeSeparator)];
+            return [.. subtype.Split(_properties.SubtypeSeparator)];
         }
 
         return [];
---
+++ MTG.Scryfall.Models/TypelineProperties.cs
@@ -0,0 +1,3 @@
+﻿namespace MTG.Scryfall.Models;
+
+public record TypelineProperties(string TypelineSeparator, string SubtypeSeparator, char[] TypeSeparators, List<string> Subtypes, List<string> Supertypes, List<string> Types);
