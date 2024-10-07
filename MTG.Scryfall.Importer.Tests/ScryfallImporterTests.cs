73a716dfb3055b27d024b0986c7af4c9fa2f1db6
Swarmotte <lagenebre.arthur@gmail.com>
2024-10-08 00:01:20 +0200

    Cleanup mess I done

--- MTG.Scryfall.Importer.Tests/ScryfallImporterTests.cs
+++ MTG.Scryfall.Importer.Tests/ScryfallImporterTests.cs
@@ -5,26 +5,7 @@ namespace MTG.Scryfall.Importer.Tests;
 
 public class ScryfallImporterTests
 {
+    private readonly IScryfallGetter _getter = Substitute.For<IScryfallGetter>();
     private readonly IScryfallMapper _mapper = Substitute.For<IScryfallMapper>();
     private readonly IScryfallReader _reader = Substitute.For<IScryfallReader>();
-
-    [Fact]
-    public void Should_Throw_FileNotFoundException_When_Path_Is_Empty()
-    {
-        // Arrange
-        var importer = new ScryfallImporter(_reader, _mapper);
-
-        // Act & Assert
-        Assert.Throws<FileNotFoundException>(() => importer.Import(string.Empty));
-    }
-
-    [Fact]
-    public void Should_Throw_FileNotFoundException_When_Path_Is_Incorrect()
-    {
-        // Arrange
-        var importer = new ScryfallImporter(_reader, _mapper);
-
-        // Act & Assert
-        Assert.Throws<FileNotFoundException>(() => importer.Import("Incorrect Path"));
-    }
 }
